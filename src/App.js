8706924afa310c95436843d1ca02045efc579286
nicolas-lamb <nikelamb@gmail.com>
2024-06-24 16:02:43 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Home from "./pages/home";
 import Movie from "./pages/movie";
+
 
 const App = () => {
     return (
         <div>
+            <nav>
+                <ol id="header">
+                    <Link to="/" class="linkLi"><li>Home</li></Link>
+                    <Link to="/" class="linkLi"><li>Mais assistidos</li></Link>
+                </ol>
+            </nav>
             <Routes>
                 <Route path="/" exact element={<Home />} />
                 <Route path="/:id" exact element={<Movie />} />
+                
+                
             </Routes>
         </div>
     );
